340e192372a56bb7b7c16763f41c5b40fe8eb81b
a-soliman <ahmed.soliman@programmer.net>
2020-05-19 08:29:32 -0700

    solved zeroOneTwo problem

--- sorting/sorting-problems/main.go
+++ sorting/sorting-problems/main.go
@@ -8,6 +8,10 @@ func copySlice(arr []int) []int {
 	return copied
 }
 
+func swap(arr []int, idx1 int, idx2 int) {
+	arr[idx1], arr[idx2] = arr[idx2], arr[idx1]
+}
+
 /*
 Portion zero one, Given an array containing 0s and 1s. Write an algorithm to sort array so that 0s come first followed by 1s.
 */
@@ -34,10 +38,29 @@ func portionZeroOne(arr []int) []int {
 Portion zero, one, and two
 Given an array containing 0s, 1s, and 2s. Write an algorithm to sort the array so that 0s come first, followed by 1s and then 2s in the end.
 */
+func portionZeroOneTwo(arr []int) []int {
+	left, right := 0, len(arr)-1
+	for i := 0; i < len(arr); i++ {
+		for arr[i] == 0 && left < i {
+			swap(arr, i, left)
+			left++
+		}
+		for arr[i] == 2 && right > i {
+			swap(arr, i, right)
+			right--
+		}
+	}
+	return arr
+}
 
 func main() {
 	zeroOneArr := []int{1, 1, 1, 0, 0, 0, 1, 1, 0, 0}
 	copied := copySlice(zeroOneArr)
 	portionedArr := portionZeroOne(copied)
-	fmt.Printf("PortionZeroOne:\nInput: %v\nOutput: %v\n", zeroOneArr, portionedArr)
+	fmt.Printf("\nPortionZeroOne:\nInput: %v\nOutput: %v\n", zeroOneArr, portionedArr)
+
+	zeroOneTwoArr := []int{1, 2, 1, 0, 2, 0, 1, 1, 2, 0}
+	copiedZeroOneTwoArr := copySlice(zeroOneTwoArr)
+	portionedArr = portionZeroOneTwo(copiedZeroOneTwoArr)
+	fmt.Printf("\nPortionZeroOneTwo:\nInput: %v\nOutput: %v\n", zeroOneTwoArr, portionedArr)
 }
