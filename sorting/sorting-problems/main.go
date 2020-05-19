7304a6df4ed71f641cc55b0c9a98d56226dbadd5
a-soliman <ahmed.soliman@programmer.net>
2020-05-19 08:18:19 -0700

    copySlice()

--- sorting/sorting-problems/main.go
+++ sorting/sorting-problems/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+func copySlice(arr []int) []int {
+	copied := make([]int, len(arr))
+	copy(copied, arr)
+	return copied
+}
+
 /*
 Portion zero one, Given an array containing 0s and 1s. Write an algorithm to sort array so that 0s come first followed by 1s.
 */
@@ -26,8 +32,7 @@ func portionZeroOne(arr []int) []int {
 
 func main() {
 	zeroOneArr := []int{1, 1, 1, 0, 0, 0, 1, 1, 0, 0}
-	copied := make([]int, len(zeroOneArr))
-	copy(copied, zeroOneArr)
+	copied := copySlice(zeroOneArr)
 	portionedArr := portionZeroOne(copied)
 	fmt.Printf("PortionZeroOne:\nInput: %v\nOutput: %v\n", zeroOneArr, portionedArr)
 }
