035476a3c4808c1209e4877c2dd041a9f42aa158
a-soliman <ahmed.soliman@programmer.net>
2020-05-24 06:33:15 -0700

    printDuplicatesInAList

--- searching/searching-problems/main.go
+++ searching/searching-problems/main.go
@@ -17,8 +17,25 @@ func firstRepeated(arr []int) int {
 	return 0
 }
 
+/*
+printDuplicatesInAList
+Given an array of n numbers, print the duplicate elements in the array
+*/
+func printDuplicates(arr []int) {
+	hash := map[int]bool{}
+	for _, num := range arr {
+		if hash[num] {
+			fmt.Println(num)
+		} else {
+			hash[num] = true
+		}
+	}
+}
+
 func main() {
 	firstRepeatedInput := []int{7, 1, 6, 3, 5, 1, 7, 4, 2}
 	firstRepeatedRes := firstRepeated(firstRepeatedInput)
 	fmt.Printf("\nFirstRepeated:\nInput: %v\nOutput: %v\n", firstRepeatedInput, firstRepeatedRes)
+
+	printDuplicates(firstRepeatedInput)
 }
