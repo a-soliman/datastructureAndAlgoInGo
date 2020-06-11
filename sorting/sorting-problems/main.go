ef9aafbe6726d66b09e9c65b48cb96429f9182b9
Ahmed Soliman <ahmed.soliman@programmer.net>
2020-06-11 07:54:45 -0700

    groupEvenOddNums

--- sorting/sorting-problems/main.go
+++ sorting/sorting-problems/main.go
@@ -342,6 +342,17 @@ func merge2SortedArrays(arr1 []int32, arr2 []int32, increasing bool) []int32 {
 	return res
 }
 
+/*
+Group the numbers
+You are given an integer array arr of size n. Group and rearrange them (in-place) into evens and odds in such a way that group of all even integers appears on the left side and group of all odd integers appears on the right side.
+Example
+Input: [1, 2, 3, 4]
+Output: [4, 2, 1, 3]
+Order does not matter. Other valid solutions are:
+[2, 4, 1, 3]
+[2, 4, 3, 1]
+[4, 2, 3, 1]
+*/
 func groupNumbers(arr []int32) []int32 {
 	size := len(arr)
 	if size < 2 {
