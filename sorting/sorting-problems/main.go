e0a3d45e33b7a3a23e8a43133b632be9a2b8c2c2
Ahmed Soliman <ahmed.soliman@programmer.net>
2020-06-11 08:47:37 -0700

    Top K

--- sorting/sorting-problems/main.go
+++ sorting/sorting-problems/main.go
@@ -379,6 +379,34 @@ func isEven(num int32) bool {
 	return num%2 == 0
 }
 
+/*
+Top K
+You are given an array of integers, arr, of size n, which is analogous to a continuous stream of integers input. Your task is to find K largest elements from a given stream of numbers.
+
+By definition, we don't know the size of the input stream. Hence, produce K largest elements seen so far, at any given time. For repeated numbers, return them only once.
+If there are less than K distinct elements in arr, return all of them.
+Don't rely on size of input array arr.
+Feel free to use built-in functions if you need a specific data-structure.
+
+
+Example One
+Input: arr = [1, 5, 4, 4, 2]; K = 2
+Output: [4, 5]
+
+Example Two
+Input: arr = [1, 5, 1, 5, 1]; K = 3
+Output: [5, 1]
+
+Notes
+
+Input Parameters: There is only one argument: Integer array arr.
+Output: Return an integer array res, containing K largest elements. If there are less than K unique elements, return all of them. Order of elements in res does not matter.
+Constraints:
+1 <= n <= 10^5
+1 <= K <= 10^5
+arr may contain duplicate numbers.
+arr may or may not be sorted
+*/
 func topK(arr []int32, k int32) []int32 {
 	size := len(arr)
 	dict := make(map[int32]bool)
