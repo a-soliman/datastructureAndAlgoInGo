afd39dc762136a5e050de2a0cf26e3aa7dbde27a
Ahmed Soliman <ahmed.soliman@programmer.net>
2020-06-19 07:58:17 -0700

    preOrder

--- trees/problems/main.go
+++ trees/problems/main.go
@@ -38,8 +38,53 @@ func btFromArrIterative(arr []int) *node {
 	return root
 }
 
+func btFromArrRecursive(arr []int) *node {
+	return btFromArrRecursiveUtil(arr, 0, len(arr))
+}
+
+func btFromArrRecursiveUtil(arr []int, idx int, size int) *node {
+	n := &node{arr[idx], nil, nil}
+	left, right := 2*idx+1, 2*idx+2
+	if left < size {
+		n.left = btFromArrRecursiveUtil(arr, left, size)
+	}
+	if right < size {
+		n.right = btFromArrRecursiveUtil(arr, right, size)
+	}
+	return n
+}
+
+/*
+PreOrderTraversal
+Perform preOrderTraversal of BT
+*/
+func preOrderTraversal(root *node) []int {
+	res := []int{}
+	traversalUtil(root, &res)
+	return res
+}
+
+func traversalUtil(node *node, res *[]int) {
+	*res = append(*res, node.value)
+	if node.left != nil {
+		traversalUtil(node.left, res)
+	}
+	if node.right != nil {
+		traversalUtil(node.right, res)
+	}
+}
+
 func main() {
 	btFromArrIterativeInput := []int{1, 2, 3, 4, 5, 6, 7}
 	btFromArrIterativeOutput := btFromArrIterative(btFromArrIterativeInput)
 	fmt.Printf("\nbtFromArrIterative: \nInput:%v\nOutput: %v\n", btFromArrIterativeInput, btFromArrIterativeOutput)
+
+	btFromArrRecursiveInput := []int{1, 2, 3, 4, 5, 6, 7}
+	btFromArrRecursiveOutput := btFromArrRecursive(btFromArrRecursiveInput)
+	fmt.Printf("\nbtFromArrRecursive: \nInput:%v\nOutput: %v\n", btFromArrRecursiveInput, btFromArrRecursiveOutput)
+
+	preOrderTraversalInput := btFromArrRecursiveOutput
+	preOrderTraversalOutput := preOrderTraversal(preOrderTraversalInput)
+	fmt.Printf("\nPreOrderTraversal:\nInput: %v\nOutput:%v\n", preOrderTraversalInput, preOrderTraversalOutput)
+
 }
