32f6c319e604babb73b63c14d6a41f344e3ce431
Ahmed Soliman <ahmed.soliman@programmer.net>
2020-06-22 18:13:18 -0700

    bst methods layout

--- trees/bst/bst.go
+++ trees/bst/bst.go
@@ -12,6 +12,12 @@ func New(value int) *BST {
 	return &BST{value, nil, nil}
 }
 
+// NewFromUnsortedSlice returns a pointer to the root of a BST built out of a given unsorted []int
+func NewFromUnsortedSlice([]int) *BST {}
+
+// NewFromSortedSlice return a pointer to the root of a BST build out of a given sorted []int
+func NewFromSortedSlice([]int) *BST {}
+
 // Insert inserts a new bst node with the given value
 func (bst *BST) Insert(value int) *BST {
 	if value <= bst.value {
@@ -29,3 +35,24 @@ func (bst *BST) Insert(value int) *BST {
 	}
 	return bst
 }
+
+// Delete deletes the first found node with the given value. returns false if no node found with the value
+func (bst *BST) Delete(value int) bool {}
+
+// Find returns a node with its value equal to a given value
+func (bst *BST) Find(value int) *BST {}
+
+// Contains returns bool represting whether a node exists with the given value
+func (bst *BST) Contains(value int) bool {}
+
+// FindMin returns a node with the min value in the tree
+func (bst *BST) FindMin() *BST {}
+
+// FindMax returns a node with the max value in the tree
+func (bst *BST) FindMax() *BST {}
+
+// FindValuesInRange return []int containing all the found values within the range
+func (bst *BST) FindValuesInRange(min, max int) []int {}
+
+// Trim given a range, it deletes all the node that are out of the range, and returns the root
+func (bst *BST) Trim(min, max int) *BST {}
