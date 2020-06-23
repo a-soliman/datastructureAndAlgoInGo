eb5e77c5cf7bb7d20351f5c302820465f8af00c4
Ahmed Soliman <ahmed.soliman@programmer.net>
2020-06-22 17:56:37 -0700

    insert()

--- trees/bst/bst.go
+++ trees/bst/bst.go
@@ -6,3 +6,26 @@ type BST struct {
 	left  *BST
 	right *BST
 }
+
+// New returns a pointer to a new bst node
+func New(value int) *BST {
+	return &BST{value, nil, nil}
+}
+
+// Insert inserts a new bst node with the given value
+func (bst *BST) Insert(value int) *BST {
+	if value <= bst.value {
+		if bst.left == nil {
+			bst.left = New(value)
+		} else {
+			bst.left.Insert(value)
+		}
+	} else {
+		if bst.right == nil {
+			bst.right = New(value)
+		} else {
+			bst.right.Insert(value)
+		}
+	}
+	return bst
+}
