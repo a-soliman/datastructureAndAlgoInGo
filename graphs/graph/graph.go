a632e2f9bf37a484bba29c54a28833a408bb491a
Ahmed Soliman <ahmed.soliman@programmer.net>
2020-06-28 06:23:02 -0700

    vertex methods

--- graphs/graph/graph.go
+++ graphs/graph/graph.go
@@ -6,6 +6,22 @@ type Vertex struct {
 	Edges map[int]*Vertex
 }
 
+func (v *Vertex) getEdges() []int {
+	keys := make([]int, 0, len(v.Edges))
+	for k := range v.Edges {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+func (v *Vertex) addEdge(toVertex *Vertex) {
+	v.Edges[toVertex.Value] = toVertex
+}
+
+func (v *Vertex) removeEdge(to int) {
+	delete(v.Edges, to)
+}
+
 // Graph type
 type Graph struct {
 	Vertices map[int]*Vertex
