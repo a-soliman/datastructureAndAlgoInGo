2f8c874db3ef109a27cbc17d489381ebea9db7cb
Ahmed Soliman <ahmed.soliman@programmer.net>
2020-07-03 18:00:41 -0700

    Zombie Clusters

--- graphs/main.go
+++ graphs/main.go
@@ -476,6 +476,50 @@ func stronglyConnectedComponentsDfsUtil(idx int, adjList *[][]int, visited *[]bo
 	*stack = append(*stack, idx)
 }
 
+/*
+Zombie Clusters
+There are zombies in Seattle. Liv and Ravi are trying to track them down to find out who is creating new zombies in an effort to prevent an apocalypse. Other than the patient-zero zombies (who became so by mixing MaxRager and tainted Utopium), new people only become zombies after being scratched by an existing zombie. Zombiism is transitive. This means that if zombie 0 knows zombie 1 and zombie 1 knows zombie 2, then zombie 0 is connected to zombie 2 by way of knowing zombie 1. A zombie cluster is a group of zombies who are directly or indirectly linked through the other zombies they know, such as the one who scratched them or supplies who them with brains.
+We have a two-dimensional array with n rows and n columns where each cell, zombies[A][B], denotes whether zombie A knows zombie B. The diagram showing connectedness will be made up of a number of binary strings, characters 0 or 1. Each of the characters in the string represents whether the zombie associated with a row element is connected to the zombie at that character's index. For instance, a zombie 0 with a connectedness string '110' is connected to zombies 0 (itself) and zombie 1, but not to zombie 2. The complete matrix of zombie connectedness is:
+
+110
+110
+001
+
+Zombies 0 and 1 are connected. Zombie 2 is not.
+Your task is to determine the number of connected groups of zombies, or clusters, in a given matrix.
+
+Example One
+Input:
+[“1100”,
+ “1110”,
+ “0110”,
+ “0001”]
+Output: 2
+*/
+func zombieCluster(zombies []string) int32 {
+	size := len(zombies)
+	visited := make([]bool, size)
+	var res int32
+	for i := 0; i < size; i++ {
+		if visited[i] == false {
+			res++
+			dfsUtil(i, zombies, &visited)
+		}
+	}
+	return res
+}
+
+func dfsUtil(idx int, zombies []string, visited *[]bool) {
+	(*visited)[idx] = true
+	neighbors := zombies[idx]
+	for neighborIdx := 0; neighborIdx < len(neighbors); neighborIdx++ {
+		if string(neighbors[neighborIdx]) == "1" && (*visited)[neighborIdx] == false {
+			(*visited)[neighborIdx] = true
+			dfsUtil(neighborIdx, zombies, visited)
+		}
+	}
+}
+
 func main() {
 	graph := graph.NewFromSlice([]int{1, 2, 3, 4, 5, 6, 7})
 	graph.AddEdge(1, 2)
