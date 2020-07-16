7fc41090adf4c44b3da3138d99e9baf984501db3
Ahmed Soliman <ahmed.soliman@programmer.net>
2020-07-16 06:42:43 -0700

    maxStockProfit

--- dynamic-programming/main.go
+++ dynamic-programming/main.go
@@ -404,6 +404,39 @@ func maxRevenue(n int, prices []int) int {
 	return revenue[n]
 }
 
+/*
+Best Time to Buy and Sell Stock
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+Note that you cannot sell a stock before you buy one.
+*/
+
+func maxStockProfit(prices []int) int {
+	size := len(prices)
+	if size <= 1 {
+		return 0
+	}
+	buy, sell, max := prices[0], prices[0], 0
+	var price int
+	var potentialMax int
+	for i := 1; i < size; i++ {
+		price = prices[i]
+		if price < buy {
+			buy = price
+			sell = price
+		}
+		if price > sell {
+			sell = price
+			potentialMax = sell - buy
+			if potentialMax > max {
+				max = potentialMax
+			}
+		}
+	}
+	return max
+}
+
 func main() {
 	fmt.Printf("FibonacciBottomUpSpaceEfficient: \nInput: %d\nOutput: %d\n", 6, fibonacciButtomUPSpaceEfficient(6))
 	fmt.Printf("\nFibonacciMemoization: \nInput: %d\nOutput: %d\n", 6, fibonacciMemoization(6))
@@ -444,4 +477,8 @@ func main() {
 
 	maxRevenueOutput := maxRevenue(5, []int{1, 5, 8, 9, 10})
 	fmt.Printf("\nMaxRevenue: %d\n", maxRevenueOutput)
+
+	maxStockProfitOutput := maxStockProfit([]int{7, 1, 5, 3, 6, 4})
+	fmt.Printf("\nMaxStockProfit: %d\n", maxStockProfitOutput)
+
 }
